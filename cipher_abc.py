00375fef3ff707104cbcc2544276415314f8be50
Patryk Młynarski <patryk.mlynarski98@gmail.com>
2025-10-01 22:06:22 +0200

    feat: work in progress, changed cipher_abc (added abstract dataclass), cipher properties also changed and added cipher from net.

--- cipher_abc.py
+++ cipher_abc.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 class CipherMethods(ABC):
     @abstractmethod
     def encrypt_data(self, input_str: str) -> str:
+        # encrypting based on ROT
         pass
 
     @abstractmethod
@@ -11,13 +12,9 @@ class CipherMethods(ABC):
         pass
 
 @dataclass
-class CipherABCData(ABC):
-    text: str | None
-    rot_type: str | None
-    status: str | None
-
-
-
-
+class CipherOutput:
+    text: str
+    rot_type: str
+    status: str
 
 
--- cipher_properties.py
+++ cipher_properties.py
@@ -1,10 +1,36 @@
-from cipher_abc import CipherABCData
+from cipher_abc import CipherMethods, CipherOutput
+from typing import Any
+import string
 
-class CipherData(CipherABCData):
-    def __init__(self, text: str, rot_type: str, status: str):
-        self.text = text
-        self.rot_type = rot_type
-        self.status = status
+class Rot17(CipherMethods):
+    def __init__(self, obj: CipherOutput):
+        self.obj = obj
+
+    def encrypt_data(self, text_to_encrypt: str) -> Any: # DataClass ROT13/ROT47:
+        text_to_encrypt = text_to_encrypt.lower()
+
+        encrypted = []
+        for char in text_to_encrypt:
+            if char.isalpha():
+                base = ord('A') if char.isupper() else ord('a')
+                new_char = chr((ord(char) - base + 17) % 26 + base)
+                encrypted.append(new_char)
+            else:
+                encrypted.append(char)
+
+        self.obj.text = text_to_encrypt
+        self.obj.rot_type = "rot17"
+        self.obj.status = 'encrypted'
+
+        return ''.join(encrypted)
+
+
+    def decrypt_data(self) -> Any:
+        pass
+
+
+
+
 
 
 
